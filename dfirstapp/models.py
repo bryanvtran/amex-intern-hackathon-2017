3ad0a9b9d0e727b9aec9753fa54baaa7ca99ce44
Bryan Tran <bryan.v.tran@aexp.com>
2017-07-13 10:31:43 -0700

    studentprofile model

--- dfirstapp/models.py
+++ dfirstapp/models.py
@@ -3,3 +3,41 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+"""
+StudentProfile
+"""
+class StudentProfile(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    bio = models.TextField()
+    picture = models.ImageField(upload_to='uploads/pictures/')
+    resume = models.FileField(upload_to='uploads/resumes/')
+
+class StudentInterest(models.Model):
+    student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE)
+    interest = models.CharField(max_length=50)
+
+class StudentSkill(models.Model):
+    student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE)
+    skill = models.CharField(max_length=50)
+
+class StudentLinks(models.Model):
+    student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE)
+    url = models.URLField()
+
+# """
+# TeamProfile
+# """
+# class TeamProfile(models.Model):
+#     name = models.StringField(max_length=30)
+#     picture
+#     description
+#     required_skills
+#
+# """
+# TeamMembers
+# """
+# class TeamMembers(models.Model):
+#     name
+#     email
+#     bio
