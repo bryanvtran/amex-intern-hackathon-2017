da2a07141beef2aa0a677b7d99676e77d70c6aa1
Amy Dao <amy.phung.dao@gmail.com>
2017-07-13 11:29:11 -0700

    Added urls and views

--- dfirstapp/views.py
+++ dfirstapp/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
-def showhome(request):
+def signup(request):
+    return
+
+def studentprof(request):
+    return render(request, 'home.html')
+
+def teamprof(request):
+    return render(request, 'home.html')
+
+def match(request):
+    strmessage =''
+    if request.method=='GET':
+        # return HttpResponse(strmessage)
+        return None
+
+    else:
+
+        # return HttpResponse(strmessage)
+        return None
+def contact(request):
     return render(request, 'home.html')
